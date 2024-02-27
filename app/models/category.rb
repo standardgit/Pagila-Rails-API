db777339d2fa1819474f1ec5f28801a8410eac3f
Peter Isaiah <peterisaiah4fun@gmail.com>
2024-02-27 23:09:12 +0100

    test customer and category

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,4 +1,6 @@
 # category model
 class Category < ApplicationRecord
   has_many :films, through: :film_categories
+
+  validates :name, presence: true
 end
--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :category do
-    name { "MyString" }
+    name { "Action" }
     last_update { "2024-02-20 17:58:46" }
   end
 end
